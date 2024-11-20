0679b614eeda1c7af63a22ef2ca1d5405107f01e
André Pilco <andrepilcoch@gmail.com>
2024-11-19 20:58:31 -0500

    finishing front

--- frontend/web.py
+++ frontend/web.py
@@ -11,13 +11,12 @@ api_key = "8455d4662cfb441db06008d90d513c5d"
 
 #Extrar los archivos pickle
 def loadModels():
-    rf_model = joblib.load('../models/regressor/rf_model_regressor.pkl')
     lgbm_model = joblib.load('../models/regressor/lgbm_model_regressor.pkl')
-    return rf_model, lgbm_model
+    return lgbm_model
     
-def get_background_image(game):
-    bg_image = requests.get(f"https://api.rawg.io/api/games/{game}?key={api_key}").json()['background_image']
-    return bg_image
+def get_game_info(game):
+    info = requests.get(f"https://api.rawg.io/api/games/{game}?key={api_key}").json()
+    return info
 
 def loadJSON():
     with open('../data/json/variables_dict.json') as json_file:
@@ -34,18 +33,89 @@ def classify(num):
     else:
         return 'Excelent'
     
+game_name_mapping = {
+    "fortnite": "Fortnite",
+    "minecraft": "Minecraft",
+    "grandTheftAuto5": "Grand Theft Auto V",
+    "apexLegends": "Apex Legends",
+    "counterStrikeGlobalOffensive": "Counter-Strike: Global Offensive",
+    "playerUnknownsBattlegrounds": "PlayerUnknown's Battlegrounds",
+    "overwatch": "Overwatch",
+    "battlefield1": "Battlefield 1",
+    "dota2": "Dota 2",
+    "leagueOfLegends": "League of Legends",
+    "rocketLeague": "Rocket League",
+    "callOfDutyBlackOps4": "Call of Duty: Black Ops 4",
+    "worldOfTanks": "World of Tanks",
+    "battlefield4": "Battlefield 4",
+    "arkSurvivalEvolved": "ARK: Survival Evolved",
+    "rust": "Rust",
+    "aWayOut": "A Way Out",
+    "airMechStrike": "AirMech Strike",
+    "battletech": "BattleTech",
+    "callOfDutyWW2": "Call of Duty: WWII",
+    "destiny2": "Destiny 2",
+    "farCry5": "Far Cry 5",
+    "frostpunk": "Frostpunk",
+    "pathOfExile": "Path of Exile",
+    "radicalHeights": "Radical Heights",
+    "rainbowSixSiege": "Rainbow Six Siege",
+    "seaOfThieves": "Sea of Thieves",
+    "starcraft2": "StarCraft II",
+    "totalWar3Kingdoms": "Total War: Three Kingdoms",
+    "warframe": "Warframe",
+}
+
+game_name_mapping_api = {
+    "Fortnite": "fortnite",
+    "Minecraft": "minecraft",
+    "Grand Theft Auto V": "grand-theft-auto-v",
+    "Apex Legends": "apex-legends",
+    "Counter-Strike: Global Offensive": "counter-strike-global-offensive",
+    "PlayerUnknown's Battlegrounds": "playerunknowns-battlegrounds",
+    "Overwatch": "overwatch",
+    "Battlefield 1": "battlefield-1",
+    "Dota 2": "dota-2",
+    "League of Legends": "league-of-legends",
+    "Rocket League": "rocket-league",
+    "Call of Duty: Black Ops 4": "call-of-duty-black-ops-4",
+    "World of Tanks": "world-of-tanks",
+    "Battlefield 4": "battlefield-4",
+    "ARK: Survival Evolved": "ark-survival-evolved",
+    "Rust": "rust",
+    "A Way Out": "a-way-out",
+    "AirMech Strike": "airmech-strike",
+    "BattleTech": "battletech",
+    "Call of Duty: WWII": "call-of-duty-wwii",
+    "Destiny 2": "destiny-2",
+    "Far Cry 5": "far-cry-5",
+    "Frostpunk": "frostpunk",
+    "Path of Exile": "path-of-exile",
+    "Radical Heights": "radical-heights",
+    "Rainbow Six Siege": "rainbow-six-siege",
+    "Sea of Thieves": "sea-of-thieves",
+    "StarCraft II": "starcraft-ii",
+    "Total War: Three Kingdoms": "total-war-three-kingdoms",
+    "Warframe": "warframe",
+}
+    
+# Update the user_input_parameters function
 def user_input_parameters(variables_dict):
+    # Sidebar inputs
     cpu_name = st.sidebar.selectbox('CPU', variables_dict['CpuName'])
     gpu_name = st.sidebar.selectbox('GPU', variables_dict['GpuName'])
     
-    game_name = st.sidebar.selectbox('Game Name', variables_dict['GameName'])
+    # Use well-written names for display but store the raw name for processing
+    raw_game_name = st.sidebar.selectbox(
+        'Game Name', variables_dict['GameName'], format_func=lambda x: game_name_mapping.get(x, x)
+    )
     game_setting = st.sidebar.selectbox('Game Setting', variables_dict['GameSetting'])
     game_resolution = st.sidebar.selectbox('Game Resolution', variables_dict['GameResolution'])
     
     cpu = variables_dict['CpuName'][cpu_name]
     gpu = variables_dict['GpuName'][gpu_name]
     
-    # Convertir todos los valores a listas
+    # Create the input data
     data = {
         'CpuNumberOfCores': [cpu['CpuNumberOfCores']],
         'CpuNumberOfThreads': [cpu['CpuNumberOfThreads']],
@@ -80,7 +150,7 @@ def user_input_parameters(variables_dict):
         'GpuTextureRate': [gpu['GpuTextureRate']],
         'GpuNumberOfTransistors': [gpu['GpuNumberOfTransistors']],
         
-        'GameName': [game_name],
+        'GameName': [raw_game_name],  # Keep the raw game name for processing
         'GameSetting': [game_setting],
         'GameResolution': [game_resolution],
                 
@@ -95,17 +165,17 @@ def user_input_parameters(variables_dict):
         'GpuAverageClock': [(gpu['GpuBaseClock'] + gpu['GpuBoostClock']) / 2],
     }
     
-    # Ahora creamos el DataFrame
+    # Return the input data and the well-written game name for display
     features = pd.DataFrame(data)
-    return features
+    return features, game_name_mapping[raw_game_name]
     
 def main():
-    st.title('Modelamiento de FPS by Andre Pilco, John Sovero e Iam Alvarez')
-    st.sidebar.header('User Input Parameters')
+    st.title('FPS calculator')
+    st.sidebar.header('Choose your hardware and game settings')
     
     variables_dict = loadJSON()
-    rf_model, lgbm_model = loadModels()
-    df = user_input_parameters(variables_dict)
+    lgbm_model = loadModels()
+    df, display_game_name = user_input_parameters(variables_dict)
     
     pipeline = loadPipeline()
     encoded = pipeline.transform(df)
@@ -148,20 +218,31 @@ def main():
        'remainder__PerformanceClockRatio', 'remainder__PerformancePerWatt',
        'remainder__CpuCoreThreadRatio', 'remainder__CpuPowerIndex',
        'remainder__GpuAverageClock'])
-    
-    option = ['Random Forest', 'LGBM']
-    model = st.sidebar.selectbox('Which model you like to use?', option)
-    
-    st.subheader('User Input Parameters')
-    st.subheader(model)
 
-    if st.button('RUN'):
-        if model == 'Random Forest':
-            prediction = rf_model.predict(data)
-            st.success(prediction)
-        elif model == 'LGBM':
-            prediction = lgbm_model.predict(data)
-            st.success(prediction)
+    if st.button('Calculate FPS'):
+        api_game_name = game_name_mapping_api[display_game_name]
+        game_info = get_game_info(api_game_name)
+        prediction = lgbm_model.predict(data)
+        # title
+        st.title(f"{display_game_name}")
+        st.image(game_info['background_image'], width=800)
+        if prediction > 100:
+            st.success(f"Predicted FPS: {prediction}")
+        elif prediction > 60:
+            st.warning(f"Predicted FPS: {prediction}")
+        else:
+            st.error(f"Predicted FPS: {prediction}")
+        st.write(f"Website: {game_info['website']}")
+        # insert html code
+        st.markdown(game_info['description'], unsafe_allow_html=True)
+        if game_info['metacritic'] != None:
+            if game_info['metacritic'] > 75:
+                st.success(f"Metacritic score: {game_info['metacritic']}")
+            elif game_info['metacritic'] > 50:
+                st.warning(f"Metacritic score: {game_info['metacritic']}")
+            else:
+                st.error(f"Metacritic score: {game_info['metacritic']}")
+        
 
 if __name__ == '__main__':
     main()
